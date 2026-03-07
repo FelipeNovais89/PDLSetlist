b250ad76a46d458ba4f8797a08022c9b84f7ed72
FelipeNovais89 <145393466+FelipeNovais89@users.noreply.github.com>
2026-03-07 09:27:06 +0000

    Update App.py

--- App.py
+++ App.py
@@ -3049,8 +3049,8 @@ def main():
     /* BACKGROUND IMAGE */
     .stApp {
         background-image: linear-gradient(
-            rgba(0,0,0,0.75),
-            rgba(0,0,0,0.85)
+            rgba(0,0,0,0.85),
+            rgba(0,0,0,0.95)
         ),
         url("https://raw.githubusercontent.com/FelipeNovais89/PDLSetlist/main/Data/IMG-20260202-WA0019.jpg");
 
